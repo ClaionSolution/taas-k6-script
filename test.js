c8005bd1d27b025e17ed624899ee2669c8da0e77
Cho HyunHee <hyhecor@gmail.com>
2024-01-17 16:29:48 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
-import { check } from 'k6';
-import http from 'k6/http';
-import { StringWithDefault } from './def.js';
+import { check } from "k6";
+import http from "k6/http";
 
+export default function () {
+  const res = http.get(__ENV.BASE_URL);
 
-const baseURL = StringWithDefault(`https://test.k6.io`)(__ENV.BASE_URL)
-
-export default function () {  
-    // console.log('BASE_URL:', baseURL)
-
-    const res = http.get(baseURL);
-
-    // console.log("response.headers:", res.headers);
-    // console.log("response.body:", res.body);
-
-	check(res, { 'check status is 200': (r) => r.status == 200 })
+  check(res, { "check status is 200": (r) => r.status == 200 });
 }
